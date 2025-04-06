c1828227b31661fb0532fcbda09d6518664a97fa
jheitert <141771200+jheitert@users.noreply.github.com>
2025-04-06 13:42:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,30 +24,6 @@ function openTab(evt, tabName) {
   }
 }
 
-
-//counter portion
-
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
-let count = 0;
-
-increaseBtn.onclick = function(){
-  count++;
-  countLabel.textContent = count;
-}
-
-decreaseBtn.onclick = function(){
-  count--;
-  countLabel.textContent = count;
-}
-
-resetBtn.onclick = function(){
-  count = 0;
-  countLabel.textContent = count;
-}
-
 function createGrid(rows = 10, cols = 10) {
   const container = document.getElementById("gridContainer");
   container.innerHTML = ""; // clear old grid
@@ -82,6 +58,28 @@ function createGrid(rows = 10, cols = 10) {
   }
 }
 
+//counter portion
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function(){
+  count++;
+  countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+  count--;
+  countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+  count = 0;
+  countLabel.textContent = count;
+}
 
 
 
