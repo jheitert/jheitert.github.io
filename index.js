1c493dfdc00a9f30059c988f6dd1002e39008bc0
jheitert <141771200+jheitert@users.noreply.github.com>
2025-04-06 11:38:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,23 +21,23 @@ resetBtn.onclick = function(){
   countLabel.textContent = count;
 }
 
-function openTab(evt, Setup) {
+function openTab(evt, tabName) {
   // Declare all variables
-  var i, setupContent, setuplinks;
+  var i, tabcontent, tablinks;
 
   // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("setupContent");
-  for (i = 0; i < setupContent.length; i++) {
-    setupContent[i].style.display = "none";
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  setuplinks = document.getElementsByClassName("setuplinks");
-  for (i = 0; i < setuplinks.length; i++) {
-    setuplinks[i].className = setuplinks[i].className.replace(" active", "");
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(setup).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 } 
